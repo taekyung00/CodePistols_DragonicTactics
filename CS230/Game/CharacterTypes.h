7e694a191246bba615465ab3567b6fd7aad8f593
seungju-digipen <seungju.s@digipen.edu>
2025-10-08 00:21:49 +0900

    typo

--- CS230/Game/CharacterTypes.h
+++ CS230/Game/CharacterTypes.h
@@ -1,10 +1,20 @@
+/*
+Copyright (C) 2023 DigiPen Institute of Technology
+Reproduction or distribution of this file or its contents without
+prior written consent is prohibited
+File Name:  CharacterTypes.h
+Project:    GAM200
+Author:     Seungju Song
+Created:    Oct 7, 2025
+*/
+
 #pragma once
 enum class CharacterTypes
 {
     None,
     Dragon,
     Fighter,
-    Rouge,
+    Rogue,
     Cleric,
     Wizard,
     Count,
