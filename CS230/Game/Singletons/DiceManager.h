aae87387fc20aa87dd0f919ff2e77d5dfc2f3990
HYW1Z <gkdldnlwm@naver.com>
2025-10-11 21:07:57 +0900

    10/11
    tlqkf

--- CS230/Game/Singletons/DiceManager.h
+++ CS230/Game/Singletons/DiceManager.h
@@ -5,14 +5,13 @@
 
 class DiceManager {
 public:
-    static DiceManager& Instance();                        // TODO 싱글톤 인스턴스 반환
-
-    int  RollDice(int count, int sides);                   // TODO count개, sides면체 주사위 합산
+    static DiceManager& Instance();
+    int  RollDice(int count, int sides);
     int  RollDiceFromString(const std::string& notation);  // TODO NdS +-M 굴리기. RollDice사용, ParseDiceString를 사용해서
                                                            // 정확한 형태인지 확인하기. if(!ParseDiceString)
                                                            // 나중에는 RollDiceFromString을 사용하지 않을 가능성도 있다. 그래도 해야함.ㅇㅇ
     //string 필요한 부분만 빼오기 --> 
-    void SetSeed(int seed);                                // TODO ?????
+    void SetSeed(int seed);
     const std::vector<int>& GetLastRolls() const;          // TODO
 
 private:
@@ -23,7 +22,7 @@ private:
     DiceManager(DiceManager&&) = delete;
     DiceManager& operator=(DiceManager&&) = delete;
 
-    static bool ParseDiceString(const std::string& s,int& count, int& sides, int& mod); //NdS +- M 을 받아서 체크하기.
+    //static bool ParseDiceString(const std::string& s,int& count, int& sides, int& mod); 없어도 되지않을까? 라는 의견...
     void LogRoll(const std::string& notation, int total) const; // TODO 주사위 결과값 로그에 출력하기. Engine::LOGGER사용.
 
 private:
