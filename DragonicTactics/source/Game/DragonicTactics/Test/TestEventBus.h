5ccb6d59c9cc51ab3246077e17c3380beb1c41ea
HYW1Z <gkdldnlwm@naver.com>
2025-12-08 02:03:45 +0900

    asd

--- DragonicTactics/source/Game/DragonicTactics/Test/TestEventBus.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-void test_subscribe_publish_singleSubscriber();
-void test_multiple_subscribers_sameEvent();
-void test_multiple_different_events();
-void test_EventData_CompleteTransfer();
-void test_EventData_MultiplePublishes();
-
-extern bool TestEventBus;
--- DragonicTactics/source/Game/DragonicTactics/Test/TestSpellSystem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-bool TestSpellRegistration();
-bool TestSpellCasting();
-bool TestSpellUpcast();
-bool TestGetAvailableSpells();
-bool TestPreviewSpellArea();
-void RunSpellSystemTests();
-
-extern bool TestSpellSystem;
