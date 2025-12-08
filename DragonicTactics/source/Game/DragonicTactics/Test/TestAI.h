560ee932ede4d8bfa642a81da838c1cc5c5b61f6
taekyung00 <103232596+taekyung00@users.noreply.github.com>
2025-12-08 14:24:20 +0900

    remove all to replace with junyoung2

--- DragonicTactics/source/Game/DragonicTactics/Test/TestAI.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-bool TestAITargetsClosestEnemy();
-bool TestAIMovesCloserWhenOutOfRange();
-bool TestAIAttacksWhenInRange();
-bool TestAIUsesShieldBashWhenAdjacent();
-bool TestAIEndsTurnWhenNoActions();
-void RunFighterAITests();
-
-extern bool TestAI;
